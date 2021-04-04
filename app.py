7c31549f2c9589bb31905447b66f0f26c3da0491
Rishavbh <rishav.b2010@gmail.com>
2021-04-04 14:57:34 +0530

    added xgboost package

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import pairwise_distances
+import xgboost as xgb
+from xgboost import XGBClassifier
 import itertools
 
 app = flask.Flask(__name__, template_folder='templates')
